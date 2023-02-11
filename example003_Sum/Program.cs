15d6f253eda31a7ddeb129cd055c42450ea8b131
andrei ignat <>
2023-02-11 16:08:50 +0200

    example003_ sum

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 33;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+
+
+
