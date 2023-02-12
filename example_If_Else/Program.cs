62364dee7fcda07c2bb29e980a30888b02991ce2
andrei ignat <>
2023-02-12 20:29:36 +0200

    example005 if else

---
+++ example_If_Else/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("введите имя пользователя : ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же МААААШААА!");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+}
